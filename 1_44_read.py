3045befb864cc49373f265e3f098d82ce2c8a435
Zichi Liao <zichi710@gmail.com>
2021-10-11 17:05:33 +0800

    calc avg length to reviews

--- 1_44_read.py
+++ 1_44_read.py
@@ -1,3 +1,13 @@
+"""
+1. 建立GitHub專案
+2. 寫程式碼來讀取留言檔
+3. 測試印出data清單
+4. 只印出第0筆看看
+5. 讀取檔案的過程中印出len(data)才知道讀取進度
+6. 建立版本上傳GitHub
+7. 算留言平均長度
+8. 建立版本上傳GitHub
+"""
 data = []
 count = 0
 with open('revies.txt', 'r') as f:
@@ -5,10 +15,16 @@ with open('revies.txt', 'r') as f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
-			print(len(data))
+			print(len(data)) #print很花時間
 
-print(len(data)) #print很花時間
+print('檔案讀取完了，總共有, 'len(data), '筆資料') 
 
 print(data[0])
 print('-----------------------------')
 print(data[1])
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print('留言的平均長度為', sum_len/len(data))
